785388e8f30d0686b736d4979fefaefdcb5d36d5
Abhishekgth <128219073+Abhishekgth@users.noreply.github.com>
2024-01-18 17:20:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	shortHex = shortHex.replace('#', '').toLowerCase();
+	
+  const red = shortHex[0];
+  const green = shortHex[1];
+  const blue = shortHex[2];
+	
+ const fullHex = `#${red}${red}${green}${green}${blue}${blue}`;
+
+  return fullHex;
 };
+
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
